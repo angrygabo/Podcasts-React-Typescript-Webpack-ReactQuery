32922451966ec3e87e3dc9903f3cc9e0bd9bcdeb
Gabriel Mendoza <gabriel.mendoza@redegal.com>
2024-03-24 22:19:06 +0100

    UPDATE - conf dev & pro mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,58 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
-module.exports = {
-  entry: './src/index.tsx',
-  mode: 'development',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    filename: 'index_bundle.js',
-    publicPath: '/'
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
-  ],
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js']
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-        ],
-      }
-    ]
-  },
-  devServer: {
-    historyApiFallback: true,
-  }
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'development';
+
+  return {
+    entry: './src/index.tsx',
+    mode: isProduction ? 'production' : 'development',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: isProduction ? 'index_bundle.[contenthash].js' : 'bundle.js',
+      publicPath: '/',
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+        minify: isProduction
+          ? {
+              collapseWhitespace: true,
+              removeComments: true,
+              removeRedundantAttributes: true,
+              removeScriptTypeAttributes: true,
+              removeStyleLinkTypeAttributes: true,
+              useShortDoctype: true,
+            }
+          : false,
+      }),
+    ],
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            'style-loader',
+            'css-loader',
+            'sass-loader'
+          ],
+        }
+      ],
+    },
+    devServer: {
+      historyApiFallback: true,
+    },
+    optimization: {
+      minimizer: isProduction ? [new TerserPlugin()] : [],
+    },
+  };
 };
